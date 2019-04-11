2cbd035eff509997e2775e55b1b2b5b7d09e235e
chtti <tzungje>
2019-04-11 17:25:25 +0800

    java training with basic object creation

---
+++ src/com/cht/training/Main6.java
@@ -0,0 +1,9 @@
+package com.cht.training;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("John");
+        Employee employee2 = new Employee("Ken");
+        Employee employee3 = new Employee("Tim");
+    }
+}
