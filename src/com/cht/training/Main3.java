8a6173b0444ca0fc6471523fba62d8a4cc761d43
chtti <tzungje>
2019-04-11 17:08:11 +0800

    java training with basic object creation

---
+++ src/com/cht/training/Main3.java
@@ -0,0 +1,14 @@
+package com.cht.training;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int x = 0b010101;
+        System.out.println("x=" + x);
+        long y = 1_000_000_001;
+        long z = 10_0000_0001;
+        System.out.println("y=" + y );
+        System.out.println("z=" + z );
+        System.out.println("y-z=" + (y - z));
+
+    }
+}
