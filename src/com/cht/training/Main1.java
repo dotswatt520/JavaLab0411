247db1d318f7696782c804df9be8d83f60c0823d
chtti <tzungje>
2019-04-11 17:07:47 +0800

    java training with basic object creation

---
+++ src/com/cht/training/Main1.java
@@ -0,0 +1,14 @@
+package com.cht.training;
+
+public class Main1 {
+    private static final String TAG1 = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("I1lO0");
+    }
+}
+
+class Foo {
+
+}
