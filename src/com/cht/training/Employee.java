671160c11b25022b76160514a6c0f99c225daa80
chtti <tzungje>
2019-04-11 17:07:21 +0800

    java training with basic object creation

---
+++ src/com/cht/training/Employee.java
@@ -0,0 +1,28 @@
+package com.cht.training;
+
+public class Employee {
+    private String name;
+
+    public Employee() {
+    } //空的建構子
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Kevin");
+        System.out.println("keven name="+emp1.getName());
+        emp1.setName("Kevin Wang");
+        System.out.println("keven name="+emp1.getName());
+    }
+}
+
