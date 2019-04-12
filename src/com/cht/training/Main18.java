672a2decab24ecf093a4b2f21373939e60e9f728
chtti <tzungje>
2019-04-12 14:54:21 +0800

    java training with basic object creation

--- src/com/cht/training/Main18.java
+++ src/com/cht/training/Main18.java
@@ -5,8 +5,8 @@ public class Main18 {
     private static String weekDays = "Sunday Monday Tuesday Wednesday Thursday Friday Saturday";
 
     public static void main(String[] args) {
-        System.out.println("find 'day':"+weekDays.indexOf(key));
-        System.out.println("last idex if :"+weekDays.lastIndexOf(key));
-        System.out.println("total length:"+weekDays.length());
+        System.out.println("find 'day':" + weekDays.indexOf(key));
+        System.out.println("last idex if :" + weekDays.lastIndexOf(key));
+        System.out.println("total length:" + weekDays.length());
     }
 }
