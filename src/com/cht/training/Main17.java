e7ffea015d3503ef5f6c8f614b103ea936d34ed6
chtti <tzungje>
2019-04-12 14:54:03 +0800

    java training with basic object creation

--- src/com/cht/training/Main17.java
+++ src/com/cht/training/Main17.java
@@ -6,5 +6,11 @@ public class Main17 {
         String s2 = new String ("Hello");
         System.out.printf("s1 == s2 %b\n" , s1 == s2);
         System.out.printf("s1 equal s2%b\n",s1.equals(s2));
+
+        String s3 = "Hello"; //p49 constant pool
+        String s4 = "Hello";
+        System.out.printf("s3 == s4 %b\n" , s3 == s4);
+        System.out.printf("s3 equal s4%b\n",s3.equals(s4));
+
     }
 }
