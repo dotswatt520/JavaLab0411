e1ff2bdf1635410c8f556101994e71c93d702c83
chtti <tzungje>
2019-04-12 14:30:25 +0800

    java training with basic object creation

--- src/com/cht/training/Main16.java
+++ src/com/cht/training/Main16.java
@@ -5,8 +5,11 @@ public class Main16 {
         Integer i1=new Integer(5);
         Integer i2=new Integer(5);
         Integer i3=i1;
+        Integer i4=new Integer(3);
         System.out.println("i1==i2?"+(i1==i2));
         System.out.println("i1==i3?"+(i1==i3));
-
+        System.out.println("i1 equal to i2?"+(i1.equals(i2)));
+        System.out.println("i1 equal to i4?"+(i1.equals(i4)));
     }
 }
+
