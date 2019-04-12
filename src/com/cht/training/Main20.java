f640587e1d98f156d9fa874ccd6d4944e3d996c0
chtti <tzungje>
2019-04-12 15:12:41 +0800

    java training with basic object creation

---
+++ src/com/cht/training/Main20.java
@@ -0,0 +1,20 @@
+package com.cht.training;
+
+public class Main20 {
+    public static void main(String[] args) {
+        boolean x1 = false;
+        int x2 = 500;
+        float x3 = 2.3f;
+        double x4 = 3.456789012345;
+        String s1 = String.valueOf(x1);
+        String s2 = String.valueOf(x2);
+        String s3 = String.valueOf(x3);
+        String s4 = String.valueOf(x4);
+
+        System.out.println("s1:" + s1);
+        System.out.println("s2:" + s2);
+        System.out.println("s3:" + s3);
+        System.out.println("s4:" + s4);
+    }
+}
+
