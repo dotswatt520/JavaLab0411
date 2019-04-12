e23d2f0ecbc0fa4d5c7c808328e072199fd6d8fc
chtti <tzungje>
2019-04-12 15:25:30 +0800

    java training with basic object creation

--- src/com/cht/training/Main20.java
+++ src/com/cht/training/Main20.java
@@ -1,5 +1,7 @@
 package com.cht.training;
 
+import javax.swing.text.StyledEditorKit;
+
 public class Main20 {
     public static void main(String[] args) {
         boolean x1 = false;
@@ -21,6 +23,16 @@ public class Main20 {
         System.out.printf("x2:%d\n",x2);
         System.out.printf("x3:%.1f\n",x3);
         System.out.printf("x4:%.12f\n",x4);
+
+        boolean x5 = Boolean.parseBoolean(s1);
+        int x6 = Integer.parseInt(s2);
+        float x7 = Float.parseFloat(s3);
+        double x8 = Double.parseDouble(s4);
+
+        System.out.printf("[back]x5:%b\n",x5);
+        System.out.printf("[back]x6:%d\n",x6);
+        System.out.printf("[back]x7:%.1f\n",x7);
+        System.out.printf("[back]x8:%.12f\n",x8);
     }
 }
 
