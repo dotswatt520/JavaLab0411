a86bbe07862f85768bff9c8682f751fddffc1e7f
chtti <tzungje>
2019-04-12 10:18:49 +0800

    java training with basic object creation

---
+++ src/com/cht/training/Lab8/OwnAccountant.java
@@ -0,0 +1,11 @@
+package com.cht.training.Lab8;
+
+public class OwnAccountant {
+    public void calculate(){
+        Employee e1 = new Employee();
+        System.out.println("a="+e1.a);
+        System.out.println("b="+e1.b);
+        System.out.println("c="+e1.c);
+        //System.out.println("d="+e1.d);
+    }
+}
